e814d4061efcdc0b6fd751f62d2fa2525bf63f2f
uhyo <uhyo@uhy.ooo>
2022-05-28 19:55:42 +0900

    add example folder

---
+++ example/src/index.js
@@ -0,0 +1,4 @@
+import * as mod from "./mod";
+
+console.log(mod.foo);
+console.log(mod);
---
+++ example/src/mod.js
@@ -0,0 +1,4 @@
+// Let's make this module tree-shakable!
+
+export const foo = "foo";
+export const bar = "bar";
