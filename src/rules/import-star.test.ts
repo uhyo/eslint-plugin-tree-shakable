f88a33ceb5d48e517014a6e1f5fae7f88709109e
uhyo <uhyo@uhy.ooo>
2022-05-28 19:25:53 +0900

    Basic function

--- src/rules/import-star.test.ts
+++ src/rules/import-star.test.ts
@@ -1,5 +1,5 @@
 import { TSESLint } from "@typescript-eslint/experimental-utils";
-import { describe, it } from "vitest";
+import { describe } from "vitest";
 import importStarRule from "./import-star";
 
 const ruleName = "import-star";
@@ -12,7 +12,7 @@ const tester = new TSESLint.RuleTester({
   },
 });
 
-describe("Property access", () => {
+describe("Basic Function", () => {
   tester.run(ruleName, importStarRule, {
     valid: [
       {
@@ -38,6 +38,56 @@ console.log(t[Math.random()]);
           {
             messageId: "non-tree-shakable-access",
             data: { module: "foo" },
+            // 't' in `t[Math.random()]`
+            line: 3,
+            column: 13,
+          },
+        ],
+      },
+      {
+        code: `
+import * as t from './aiu';
+console.log(t[Math.random() ? "foo" : "bar" ]);
+`,
+        errors: [
+          {
+            messageId: "non-tree-shakable-access",
+            data: { module: "./aiu" },
+            // 't' in `t[...]`
+            line: 3,
+            column: 13,
+          },
+        ],
+      },
+      {
+        code: `
+import * as t from './aiu';
+const obj = t;
+console.log(obj.foo);
+`,
+        errors: [
+          {
+            messageId: "non-tree-shakable-access",
+            data: { module: "./aiu" },
+            // 't' in `const obj = t`
+            line: 3,
+            column: 13,
+          },
+        ],
+      },
+      {
+        code: `
+import * as t from './aiu';
+// Webpack cannot handle this case
+console.log(t[\`foo\`]);
+`,
+        errors: [
+          {
+            messageId: "non-tree-shakable-access",
+            data: { module: "./aiu" },
+            // 't' in `const obj = t`
+            line: 4,
+            column: 13,
           },
         ],
       },
--- src/rules/import-star.ts
+++ src/rules/import-star.ts
@@ -1,4 +1,4 @@
-import { TSESLint } from "@typescript-eslint/experimental-utils";
+import { TSESLint, TSESTree } from "@typescript-eslint/experimental-utils";
 
 type MessageIds = "non-tree-shakable-access";
 
@@ -17,8 +17,73 @@ const importStarRule: TSESLint.RuleModule<MessageIds> = {
     schema: [],
   },
   create: function (context) {
-    return {};
+    return {
+      ImportNamespaceSpecifier(node) {
+        // import * as mod from 'specifier'
+        const identifier = node.local;
+        const moduleName = getModuleSpecifier(node);
+        checkModuleNamespaceUsage(context, moduleName, identifier);
+      },
+    };
   },
 };
 
 export default importStarRule;
+
+function getModuleSpecifier(node: TSESTree.ImportNamespaceSpecifier): string {
+  const parent = node.parent;
+  if (parent === undefined) {
+    throw new Error("Cannot find parent of ImportNamespaceSpecifier");
+  }
+  if (parent.type !== TSESTree.AST_NODE_TYPES.ImportDeclaration) {
+    throw new Error(
+      "Cannot find ImportDeclaration for given ImportNamespaceSpecifier"
+    );
+  }
+  return parent.source.value;
+}
+
+function checkModuleNamespaceUsage(
+  context: Readonly<TSESLint.RuleContext<MessageIds, unknown[]>>,
+  moduleName: string,
+  identifier: TSESTree.Identifier
+) {
+  const scope = context.getScope();
+  const variable = scope.set.get(identifier.name);
+  for (const ref of variable.references) {
+    const referencedIdentifier = ref.identifier;
+    if (
+      referencedIdentifier.type !== TSESTree.AST_NODE_TYPES.Identifier ||
+      !isTreeShakingSafeReference(referencedIdentifier)
+    ) {
+      context.report({
+        node: referencedIdentifier,
+        messageId: "non-tree-shakable-access",
+        data: {
+          module: moduleName,
+        },
+      });
+    }
+  }
+}
+
+function isTreeShakingSafeReference(identifier: TSESTree.Identifier): boolean {
+  // Only allow `id.foo` or `id["foo"]` references.
+  const parent = identifier.parent;
+  if (parent === undefined) {
+    return false;
+  }
+
+  if (parent.type === TSESTree.AST_NODE_TYPES.MemberExpression) {
+    if (!parent.computed) {
+      // id.foo
+      return true;
+    }
+    const memberName = parent.property;
+    if (memberName.type === TSESTree.AST_NODE_TYPES.Literal) {
+      // id["foo"]
+      return true;
+    }
+  }
+  return false;
+}
